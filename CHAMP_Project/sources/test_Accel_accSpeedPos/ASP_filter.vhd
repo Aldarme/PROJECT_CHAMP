9279299237ef20e3461242ff86278f0e3608fcd4
Unknown <rometpierre@gmail.com>
2018-11-13 19:02:52 +0100

    ASP - functional avrg acceleration

--- CHAMP_Project/sources/test_Accel_accSpeedPos/ASP_filter.vhd
+++ CHAMP_Project/sources/test_Accel_accSpeedPos/ASP_filter.vhd
@@ -42,7 +42,7 @@ architecture filter_mapBitAvrgANDInteg of ASP_filter is
  
  signal offset : signed(16 downto 0) := 17x"08000";		--offset of 32768 (8000 Hex) to transform signed word into an unsigned word
 
- type   T_SPISTATE is ( IDLEst, Testst, CALIBst, Mapst, TRANSMITst);
+ type   T_SPISTATE is ( IDLEst, Testst, CALIBst, MAPst, ADDst, TRANSMITst);
  signal cState	: T_SPISTATE;
  signal cState2	:	T_SPISTATE;
  signal cState3 : T_SPISTATE;
@@ -50,13 +50,17 @@ architecture filter_mapBitAvrgANDInteg of ASP_filter is
  
  signal tmp17b		: signed(16 downto 0);
  signal signedTmp	: signed(16 downto 0);
+ signal avrg17b		: signed(16 downto 0);
+ signal avgSgnTmp	: signed(16 downto 0);
+ signal speedAdd 	: signed(16 downto 0);
+ signal sgnSpdTmp	: signed(16 downto 0);
  
- type myTab is array ( 0 to 1023) of std_logic_vector(19 downto 0); -- depth of the average array 1024 or 2048
+ type myTab is array(0 to 1023) of std_logic_vector(15 downto 0); -- depth of the average array 1024 or 2048
  
  signal NBits		: integer := 1024;				-- Array delimiter
  signal myData	: myTab;									-- Array of successive value of acceleration
  signal movIdx	:	integer	:= 0;						-- Most old value index of the acceleration array
- signal avrg		:	signed(19 downto 0);		-- Calculated average
+ signal avrg		:	signed(15 downto 0);		-- Calculated average
  
  component ASP_HexDisplay
 	port
@@ -104,7 +108,7 @@ architecture filter_mapBitAvrgANDInteg of ASP_filter is
 				
 			when Testst =>
 				
-				tmp17b <= resize(signed(RCV_TOFILTER(19 downto 4)) - avrg, tmp17b'length);
+				tmp17b <= resize(signed(RCV_TOFILTER(19 downto 4)), tmp17b'length);
 				cState 	<= CALIBst;
 				
 			when CALIBst	=>
@@ -115,7 +119,7 @@ architecture filter_mapBitAvrgANDInteg of ASP_filter is
 			when TRANSMITst =>
 				
 				FLT_OE_OUTPUT <= '1';
-				TSMT_TOANALOG <= std_logic_vector(unsigned(signedTmp(15 downto 0)));
+				TSMT_TOANALOG <= std_logic_vector(unsigned(signedTmp(15 downto 0)) - unsigned(avrg));
 				cState 	<= IDLEst;
 				
 			when others	=>
@@ -138,11 +142,11 @@ architecture filter_mapBitAvrgANDInteg of ASP_filter is
 		if RESET_SIGNAL = '0' then
 			
 			for I in 0 to 1023 loop
-				myData(I) <= 20x"0";
+				myData(I) <= 16x"0";
 			end loop;
 			
 			movIdx	<= 0;
-			avrg		<= 20x"0";
+			avrg		<= 16x"0";
 			
 			cState2	<= IDLEst;
 			
@@ -160,12 +164,21 @@ architecture filter_mapBitAvrgANDInteg of ASP_filter is
 					
 				when Testst =>
 					
-					avrg <= avrg + signed(RCV_TOFILTER) - signed(myData(movIdx));
-					myData(movIdx) <= RCV_TOFILTER;
+					avrg17b <= resize(signed(RCV_TOFILTER(19 downto 4)), avrg17b'length);
+					cState2 	<= CALIBst;
 					
-					cState2 <= CALIBst;
+				when CALIBst	=>
 					
-				when CALIBst =>
+					avgSgnTmp <= avrg17b + offset;
+					cState2 	<= MAPst;
+					
+				when MAPst =>
+					
+					avrg <= avrg + avgSgnTmp(15 downto 0) - signed(myData(movIdx));
+					myData(movIdx) <= std_logic_vector(unsigned(avgSgnTmp(15 downto 0)));
+					cState2 <= TRANSMITst;
+					
+				when TRANSMITst =>
 					
 					if movIdx >= NBits -1 then
 						movIdx <= 0;
@@ -187,10 +200,8 @@ architecture filter_mapBitAvrgANDInteg of ASP_filter is
  --
  dp_dt: process(RESET_SIGNAL, CLOCK_50) is
 			
-		variable speedAdd : signed(19 downto 0);
+	begin
 			
-	 begin
-	 
 		if RESET_SIGNAL = '0' then
 			
 			SPD_OUTPUT 		<= 16x"0";
@@ -204,28 +215,29 @@ architecture filter_mapBitAvrgANDInteg of ASP_filter is
 				
 				when IDLEst =>
 					
+					SPD_OE_OUTPUT <= '0';
+					
 					if flt_OE_INPUT = '1' then
-						cState3 <= Mapst;
+						cState3 <= CALIBst;
 					else
 						cState3 <= IDLEst;
 					end if;
 					
-				when Mapst =>
+				when CALIBst =>
 					
-					speedAdd		:= signed(RCV_TOFILTER) + signed(SPD_OUTPUT);	
-					SPD_OUTPUT	<= std_logic_vector(speedAdd(19 downto 4));
-					SPD_COUNT		<= SPD_COUNT + 1;
+					speedAdd <= resize(signed(RCV_TOFILTER(19 downto 4)), speedAdd'length);
+					cState3 <= MAPst;
 					
+				when MAPst =>
+					
+					sgnSpdTmp	<= speedAdd + offset;
+					SPD_OE_OUTPUT <= '1';
 					cState3 <= TRANSMITst;
 					
 				when TRANSMITst =>
 					
-					SPD_OE_OUTPUT <= '1';
-					cState3 <= CALIBst;
-					
-				when CALIBst =>
-					
-					SPD_OE_OUTPUT <= '0';
+					SPD_OUTPUT	<= std_logic_vector(unsigned(sgnSpdTmp(15 downto 0) + signed(SPD_OUTPUT)));
+					SPD_COUNT		<= SPD_COUNT + 1;
 					cState3 <= IDLEst;
 					
 				when others =>
@@ -254,6 +266,8 @@ architecture filter_mapBitAvrgANDInteg of ASP_filter is
 				
 				when IDLEst =>
 					
+					POS_OE_OUTPUT <= '0';
+					
 					if SPD_OE_OUTPUT = '1' then
 						cState4 <= Mapst;
 					else
@@ -262,22 +276,14 @@ architecture filter_mapBitAvrgANDInteg of ASP_filter is
 					
 				when Mapst =>
 					
-					POS_OUTPUT <= std_logic_vector(signed(SPD_OUTPUT) + signed(POS_OUTPUT));
-					POS_COUNT	 <= POS_COUNT +1;
-					
+					POS_OE_OUTPUT <= '1';
 					cState4 <= TRANSMITst;
 					
 				when TRANSMITst =>
 					
-					POS_OE_OUTPUT <= '1';
-					
+					POS_OUTPUT <= std_logic_vector(unsigned(SPD_OUTPUT) + unsigned(POS_OUTPUT));
+					POS_COUNT	 <= POS_COUNT +1;
 					cState4 <= Testst;
-					
-				when Testst =>
-					
-					POS_OE_OUTPUT <= '0';
-					
-					cState4 <= IDLEst;
 					
 				when others =>
 					cState4 <= IDLEst;
