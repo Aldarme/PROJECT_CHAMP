8afd6efe29e50edbe6f0a1b5d1885bad07c00987
Unknown <rometpierre@gmail.com>
2018-11-26 20:53:46 +0100

    ASP - filter integrate speed & pos - still cnt bug to fix

--- CHAMP_Project/sources/test_Accel_accSpeedPos/ASP_filter.vhd
+++ CHAMP_Project/sources/test_Accel_accSpeedPos/ASP_filter.vhd
@@ -55,8 +55,8 @@ architecture filter_mapBitAvrgANDInteg of ASP_filter is
  
  constant offset : signed(16 downto 0) := 17x"08000";		--offset of 32768 (8000 Hex) to transform signed word into an unsigned word
  
--- type myTab is array(0 to 1023) of std_logic_vector(15 downto 0); -- depth of the average array 1024 or 2048
- type   T_SPISTATE is ( IDLEst, Testst, CALIBst, MAPst, REMAPst, ADDst, checkMaxst, NORMALIZEst, TRANSMITst);
+ type myTab is array(0 to 4) of unsigned(15 downto 0);
+ type T_SPISTATE is ( IDLEst, Testst, CALIBst, MAPst, REMAPst, ADDst, checkMaxst, NORMALIZEst, TRANSMITst);
 
  signal cState	: T_SPISTATE;
  signal cState2 :	T_SPISTATE;
@@ -65,25 +65,26 @@ architecture filter_mapBitAvrgANDInteg of ASP_filter is
  
  --acceleration
  signal tmp17b		: signed(16 downto 0);
- signal signedTmp	: signed(16 downto 0);
+ signal signedTmp : signed(16 downto 0);
+ signal tmp 			: unsigned(15 downto 0);
+ 
  --average
--- signal NBits	  : integer := 1024;			-- Array delimiter
--- signal myData	: myTab;								-- Array of successive value of acceleration
--- signal movIdx	:	integer	:= 0;					-- Most old value index of the acceleration array
  signal avgSgnTmp	: signed(16 downto 0);
  signal normVal 	: unsigned(15 downto 0);
- signal avrg		:	signed(15 downto 0);		-- Calculated average
- signal avgAdder:	signed(15 downto 0);		-- Calculated average
- signal avgIdx	: integer := 0;
+ signal avrg			:	signed(23 downto 0);		-- Calculated average
+ signal avgAdder	:	signed(23 downto 0);		-- Calculated average
  --speed
- signal sgnSpdTmp	: integer; --range 0 to 2**16-1;
- signal lessOffset: integer; --range -2**15 to 2**15-1;
- signal speedAdder: integer; --range -2**15 to 2**15-1;
- signal SpdTmpadd	: integer; --range 0 to 2**16-1;
- --	signal speedAdBuf: inte;ger range -2**15 to 2**15-1;
- --position
- signal withoffs  : integer;
- signal posAdder  : integer;
+ signal spdTab 		: myTab;
+ signal spdIdx	 	: integer;
+ signal spdAdder	: unsigned(20 downto 0);
+ signal spdDiv		: unsigned(20 downto 0);
+ signal to_send		:	unsigned(16 downto 0);
+--position
+ signal posTab 		: myTab;
+ signal posIdx	 	: integer;
+ signal posAdder	: unsigned(20 downto 0);
+ signal posDiv		: unsigned(20 downto 0);
+ signal pos_send	:	unsigned(16 downto 0);
  
  component ASP_HexDisplay
 	port
@@ -110,7 +111,7 @@ architecture filter_mapBitAvrgANDInteg of ASP_filter is
 -- Acceleration data transmit
 --	data transmit = acceleration - average acceleration
 --
-flt: process(RESET_SIGNAL, CLOCK_50) is
+Acceleration: process(RESET_SIGNAL, CLOCK_50) is
   begin
 	if RESET_SIGNAL = '0' then
 		
@@ -142,11 +143,16 @@ flt: process(RESET_SIGNAL, CLOCK_50) is
 			when CALIBst	=>
 				
 				signedTmp <= tmp17b + offset;
+				cState 	<= MAPst;
+				
+			when MAPst =>
+				
+				tmp <= unsigned(avgSgnTmp(15 downto 0)) - to_unsigned(32768, tmp'length );
 				cState 	<= NORMALIZEst;
 				
 			when NORMALIZEst =>
 				
-				normVal <= unsigned(signedTmp(15 downto 0)) - (unsigned(avgSgnTmp(15 downto 0) - 32768));
+				normVal <= unsigned(signedTmp(15 downto 0)) - (tmp);
 				cState 	<= TRANSMITst;
 				
 			when TRANSMITst =>
@@ -160,27 +166,22 @@ flt: process(RESET_SIGNAL, CLOCK_50) is
 				
 		end case;
 	end if;
-end process flt;
+end process Acceleration;
  
  
 --
--- Slidding window average
---
---	The average data (avrg) calculated below, is define has "signed"
+-- Average acceleration, measured on 1024 samples (or ~1sec)
 --
 average: process(RESET_SIGNAL, CLOCK_50) is
+		
+		variable avgIdx	: integer := 0;
 		
 	begin
 		
 	if RESET_SIGNAL = '0' then
-		
---		for I in 0 to 1023 loop
---			myData(I) <= 16x"0";
---		end loop;
-		--movIdx	<= 0;
-		avrg		 <= 16x"0";
-		avgAdder <= 16x"0";
-		avgIdx	 <= 0;
+		avrg		 <= 24x"0";
+		avgAdder <= 24x"0";
+		avgIdx	 := 0;
 		avgSgnTmp <= 17x"0";
 		cState2	<= IDLEst;
 		
@@ -202,7 +203,7 @@ average: process(RESET_SIGNAL, CLOCK_50) is
 						AND to_integer(signed(RCV_TOFILTER(19 downto 4))) <= 4096 then
 						
 						avgAdder <= avgAdder + signed(RCV_TOFILTER(19 downto 4));
-						avgIdx <= avgIdx +1;
+						avgIdx := avgIdx +1;
 						cState2 <= CALIBst;
 					else
 						cState2 <= IDLEst;
@@ -211,43 +212,17 @@ average: process(RESET_SIGNAL, CLOCK_50) is
 				when CALIBst =>
 					
 					if avgIdx >= 1024 then
-						avrg		<= shift_right(signed(avgAdder), 10);
-						avgIdx	<= 0;
-						cState2 <= TRANSMITst;
+						avrg		 <= shift_right(signed(avgAdder), 10);
+						avgAdder <= 24x"0";
+						avgIdx	 := 0;
+						cState2	 <= TRANSMITst;
 					else
 						cState2 <= IDLEst;
 					end if;
 					
 				when TRANSMITst =>
-					avgSgnTmp <= avrg + offset;
+					avgSgnTmp <= avrg(15 downto 0) + offset;
 					cState2 <= IDLEst;
-				
-				
---			when Testst =>
---				
---				if to_integer(signed(RCV_TOFILTER(19 downto 4))) >= 0
---					 AND to_integer(signed(RCV_TOFILTER(19 downto 4))) <= 4096 then
---						
---					avrg <= avrg + signed(RCV_TOFILTER(19 downto 4)) - signed(myData(movIdx));
---					myData(movIdx) <= RCV_TOFILTER(19 downto 4);
---					cState2 <= CALIBst;
---				else
---					cState2 <= IDLEst;
---				end if;
---				
---			when CALIBst	=>
---				
---				avgSgnTmp <= avrg + offset;	--renvoie valeur non-signé, ex: 0.5g -> 2048 -> 34816
---				cState2 	<= TRANSMITst;
---				
---			when TRANSMITst =>
---				
---				if movIdx >= NBits -1 then
---					movIdx <= 0;
---				else
---					movIdx <= movIdx +1;
---				end if;
---				cState2 <= IDLEst;
 				
 			when others =>
 				cState2 <= IDLEst;
@@ -256,164 +231,166 @@ average: process(RESET_SIGNAL, CLOCK_50) is
 end process average;
 
 	
--- --
--- -- Integrated acceleration
--- --
--- dp_dt: process(RESET_SIGNAL, CLOCK_50) is	--IDLEst, Testst, CALIBst, MAPst, REMAPst, ADDst, TRANSMITst, checkMaxst
---			
---			variable tmpLess : integer;
---			variable gain		 : integer;
---			
---	begin
---			
---		if RESET_SIGNAL = '0' then
---			
---			SPD_OUTPUT 		<= 16x"0";
---			SPD_OE_OUTPUT <= '0';
---			SPD_COUNT			<= 0;
---			sgnSpdTmp			<= 0;--17x"0";
---			lessOffset		<= 0;
---			--speedAdBuf		<= 0;--17x"0";17x"0";
---			SpdTmpadd			<= 0;--17x"0";17x"0";
---			speedAdder		<= 0;--17x"0";17x"0";
---			tmpLess				:= 0;
---			gain					:= 0;
---			cState3 			<= IDLEst;
---			
---		elsif rising_edge(CLOCK_50) then
---				
---			case cState3 is
---				
---				when IDLEst =>
---						
---					SPD_OE_OUTPUT <= '0';
---						
---					if en3 = '1' then
---						cState3 <= Testst;
---					else
---						cState3 <= IDLEst;
---					end if;
---					
---				when Testst =>
---					
---					sgnSpdTmp	<= to_integer(unsigned(signedTmp(15 downto 0)) - unsigned(avrg));
---					cState3 <= CALIBst;
---					
---				when CALIBst =>
---					
---					lessOffset <= sgnSpdTmp - 32768;
---					cState3 <= REMAPst;
---					
---				when REMAPst =>
---					
---					if lessOffset > -4096 AND lessOffset < 4096 then --au repos, marge de +-1g pour remise à zero
---						tmpLess := 0;
---						speedAdder <= 0;
---						gain := 0;
---					else
---						tmpLess := lessOffset;
---						gain 		:= 10000;
---					end if;
---					
---					cState3 <= ADDst;
---					
---				when ADDst =>
---					
-----					speedAdder <= speedAdder + (tmpLess/1000);
---					speedAdder <= speedAdder + (tmpLess/1024);
---					cState3 <= MAPst;
---					
---				when MAPst =>
---					
---					SpdTmpadd <= speedAdder + 32768 ;--+ gain
---					
---					cState3 <= checkMaxst;
---					
---				when checkMaxst =>
---					
---					if SpdTmpadd > 65535 then
---						SpdTmpadd <= 65535;
---					end if;
---					
---					
---					cState3 <= TRANSMITst;
---					
---				when TRANSMITst =>
---					
---					SPD_OUTPUT	<= std_logic_vector(to_unsigned(SpdTmpadd, 16));
---					SPD_COUNT		<= SPD_COUNT + 1;
---					SPD_OE_OUTPUT <= '1';
---					cState3 <= IDLEst;
---					
---				when others =>
---					cState3 <= IDLEst;
---			end case;
---		end if;	
---	end process dp_dt;
--- 
--- 
--- --
--- -- Integrated speed
--- --
--- dv_dt: process(RESET_SIGNAL, CLOCK_50) is
---		
---		variable tmpLess : integer;
---		
---	 begin
---		
---		if  RESET_SIGNAL = '0' then
---			
---			POS_OUTPUT		<= 16x"0";
---			POS_OE_OUTPUT	<= '0';
---			POS_COUNT			<= 0;
---			cState4 <= IDLEst;
---			
---		elsif rising_edge(CLOCK_50) then
---			
---			case cState4 is
---				
---				when IDLEst =>
---					
---					POS_OE_OUTPUT <= '0';
---					
---					if en4 = '1' then
---						cState4 <= Testst;
---					else
---						cState4 <= IDLEst;
---					end if;
---					
---				when Testst =>
---					
---					if speedAdder > -1000 AND speedAdder < 1000 then
---						tmpLess := 0;
---						posAdder <= 0;
---					else
---						tmpLess := speedAdder;
---					end if;					
---					cState4 <= CALIBst;
---					
---				when CALIBst =>
---					
---					posAdder <= tmpLess + posAdder;					
---					cState4 <= ADDst;
---					
---				when ADDst =>
---					
---					withoffs <= posAdder + to_integer(offset);
---					cState4 <= TRANSMITst;
---					
---				when TRANSMITst =>
---					
---					POS_OUTPUT <= std_logic_vector(to_unsigned(withoffs, 16));
---					POS_COUNT	 <= POS_COUNT +1;
---					POS_OE_OUTPUT <= '1';
---					cState4 <= Testst;
---					
---				when others =>
---					cState4 <= IDLEst;
---			end case;			
---		end if; 
---	end process dv_dt;
+--
+-- Integrated acceleration
+--
+Speed: process(RESET_SIGNAL, CLOCK_50) is	
+-- signal spdTab 		: myTab;
+-- signal spdIdx	 	: integer;
+-- signal spdAdder	: unsigned(20 downto 0);
+-- signal spdDiv		: unsigned(16 downto 0);
+-- signal to_send		:	unsigned(16 downto 0;
+		
+begin
+		
+	if RESET_SIGNAL = '0' then
+		
+		for I in 0 to spdTab'length-1 loop
+			spdTab(I) <= 16x"0";
+		end loop;
+		spdIdx				<= 0;
+		spdAdder			<= 21x"0";
+		spdDiv				<= 21x"0";
+		to_send				<= 17x"0";
+		SPD_OUTPUT 		<= 16x"0";
+		SPD_OE_OUTPUT <= '0';
+		SPD_COUNT			<= 0;
+		cState3 			<= IDLEst;
+		
+	elsif rising_edge(CLOCK_50) then
+			
+		case cState3 is
+			
+			when IDLEst =>
+					
+				SPD_OE_OUTPUT <= '0';
+					
+				if FLT_OE_OUTPUT = '1' then
+					cState3 <= Testst;
+				else
+					cState3 <= IDLEst;
+				end if;
+				
+			when Testst =>
+				
+				if unsigned(TSMT_TOANALOG) > to_unsigned(36864, TSMT_TOANALOG'length) then
+					spdTab(spdIdx) <= unsigned(TSMT_TOANALOG) - to_unsigned(36864, TSMT_TOANALOG'length);
+				else
+					spdTab(spdIdx) <= 16x"0";
+				end if;
+				spdIdx 	<= spdidx +1;
+				cState3 <= ADDst;
+				
+			when ADDst =>
+				if spdIdx = 5 then
+					spdIdx <= 0;
+				end if;
+				
+				spdAdder <= resize(spdTab(0) + spdTab(1) + spdTab(2) + spdTab(3) + spdTab(4) ,spdAdder'length);
+				cState3 <= MAPst;
+				
+			when MAPst =>
+				spdDiv 	<= shift_right(unsigned(spdAdder), 2);
+				cState3 <= REMAPst;
+				
+			when REMAPst =>
+				
+				to_send <= spdDiv(16 downto 0) + to_unsigned(32768, to_send'length);
+				cState3 <= TRANSMITst;
+				
+			when TRANSMITst =>
+				
+				SPD_OUTPUT	<= std_logic_vector(to_send(15 downto 0));
+				SPD_COUNT		<= SPD_COUNT + 1;
+				SPD_OE_OUTPUT <= '1';
+				cState3 <= IDLEst;
+				
+			when others =>
+				cState3 <= IDLEst;
+		end case;
+	end if;	
+end process Speed;
+ 
+ 
+--
+-- Integrated speed
+--
+Position: process(RESET_SIGNAL, CLOCK_50) is
+-- signal posTab 		: myTab;
+-- signal posIdx	 	: integer;
+-- signal posAdder	: unsigned(20 downto 0);
+-- signal posDiv		: unsigned(16 downto 0);
+-- signal pos_send		:	unsigned(16 downto 0;
+		
+begin
+		
+	if RESET_SIGNAL = '0' then
+		
+		for I in 0 to posTab'length-1 loop
+			posTab(I) <= 16x"0";
+		end loop;
+		posIdx				<= 0;
+		posAdder			<= 21x"0";
+		posDiv				<= 21x"0";
+		pos_send			<= 17x"0";
+		POS_OUTPUT		<= 16x"0";
+		POS_OE_OUTPUT	<= '0';
+		POS_COUNT			<= 0;
+		cState4 			<= IDLEst;
+		
+	elsif rising_edge(CLOCK_50) then
+			
+		case cState4 is
+			
+			when IDLEst =>
+					
+				POS_OE_OUTPUT <= '0';
+					
+				if SPD_OE_OUTPUT = '1' then
+					cState4 <= Testst;
+				else
+					cState4 <= IDLEst;
+				end if;
+				
+			when Testst =>
+				
+				if unsigned(SPD_OUTPUT) > to_unsigned(36864, SPD_OUTPUT'length) then
+					posTab(posIdx) <= unsigned(SPD_OUTPUT) - to_unsigned(36864, SPD_OUTPUT'length);
+				else
+					posTab(posIdx) <= 16x"0";
+				end if;
+				posIdx 	<= posIdx +1;
+				cState4 <= ADDst;
+				
+			when ADDst =>
+				if posIdx = 5 then
+					posIdx <= 0;
+				end if;
+				
+				posAdder <= resize(posTab(0) + posTab(1) + posTab(2) + posTab(3) + posTab(4) ,posAdder'length);
+				cState4 <= MAPst;
+				
+			when MAPst =>
+				posDiv 	<= shift_right(unsigned(posAdder), 2);
+				cState4 <= REMAPst;
+				
+			when REMAPst =>
+				
+				pos_send <= posDiv(16 downto 0) + to_unsigned(32768, pos_send'length);
+				cState4 <= TRANSMITst;
+				
+			when TRANSMITst =>
+				
+				POS_OUTPUT	<= std_logic_vector(pos_send(15 downto 0));
+				POS_COUNT		<= POS_COUNT + 1;
+				POS_OE_OUTPUT <= '1';
+				cState4 <= IDLEst;
+				
+			when others =>
+				cState4 <= IDLEst;
+		end case;
+	end if;	
+end process Position;
 
 end filter_mapBitAvrgANDInteg;
 
